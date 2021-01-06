771feead4b0e400f638e09a07d6686ced5d4b227
Kodyx <66336236+kodyx27@users.noreply.github.com>
2021-01-06 20:12:32 +0300

    Update yaz.js

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -1,5 +1,20 @@
 const Discord = require('discord.js');
 
+
+////////////////////////////////////////////////
+exports.run = function(client, message, args) {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    const embed = new Discord.RichEmbed()
+      .setColor("BLUE")
+      .setDescription(`Ne yazıkki bu komutu kullanmaya yetkiniz yok!`)
+      .setFooter(client.user.username, client.user.avatarURL);
+
+    message.channel.send(embed);
+    return;
+  }
+  
+  
+
 exports.run = (client, message, args) => {
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
